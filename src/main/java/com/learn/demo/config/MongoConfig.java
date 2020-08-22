8d1ad532fc0297381c11195afcef4cb2a333d31a
jiaweiban <jiaweiban@163.com>
2020-08-22 11:52:51 +0800

    增加可选配置文件配置数据源方式

--- src/main/java/com/learn/demo/config/MongoConfig.java
+++ src/main/java/com/learn/demo/config/MongoConfig.java
@@ -5,12 +5,22 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongoConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+    @Value("${spring.data.mongodb.username}")
+    private String userName;
+    @Value("${spring.data.mongodb.password}")
+    private String password;
 
     public @Bean MongoTemplate mongoTemplate() {
         return new MongoTemplate(mongoClient(), "test");
@@ -20,9 +30,9 @@ public class MongoConfig {
         return MongoClients.create(MongoClientSettings
                 .builder()
                 //链接地址
-                .applyConnectionString(new ConnectionString("mongodb://10.10.10.102:37017"))
+                .applyConnectionString(new ConnectionString(uri))
                 //用户名密码
-                .credential(MongoCredential.createCredential("test", "test", "123456".toCharArray()))
+                .credential(MongoCredential.createCredential(userName, database, password.toCharArray()))
                 .build());
     }
 }
